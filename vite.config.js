0a5e7b9a70d9b7c7493f70b5031ca13578621077
philbgarner <phil.b.garner@gmail.com>
2026-04-04 08:48:51 -0400

    Changing config slightly so it will run on itch.io.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/teaproto/',
+  base: "",
   plugins: [react()],
   resolve: {
-    dedupe: ['react', 'react-dom', '@react-three/fiber', '@react-three/drei', 'three'],
+    dedupe: [
+      "react",
+      "react-dom",
+      "@react-three/fiber",
+      "@react-three/drei",
+      "three",
+    ],
   },
-})
+});
